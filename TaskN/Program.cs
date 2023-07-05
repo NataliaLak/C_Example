0b859a2709015378868e9eaa3516369fa3598793
nataliaL <sta491@yandex.ru>
2023-07-05 16:44:38 +0300

    Add numbers N, -N

---
+++ TaskN/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите число N: ");
+int n = int.Parse(Console.ReadLine());
+n = Math.Abs(n);
+
+Console.WriteLine($"Целые числал в промежутке от -{n} до {n}: ", n);
+
+for (int i = -n; i <= n; i++)
+{
+    Console.Write(i+", ");
+}
+Console.Write(n);
