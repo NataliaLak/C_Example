2ce9cc0cbddef8d9689a33d0beec47bcac1d9b7d
nataliaL <sta491@yandex.ru>
2023-07-05 14:08:00 +0300

    add Sqr Conditions IfElse

---
+++ TaskSqr1/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите первое число: ");
+int number1 = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int number2 = int.Parse(Console.ReadLine());
+
+if (number1 == number2*number2)
+{
+    Console.WriteLine($"Число {number1} является квадратом числа: " + number2);
+}
+else
+    Console.WriteLine($"Число {number1} не является квадратом числа: " + number2);
