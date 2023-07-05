d725c15ed9a8d2a2fd1f762531771b61ed3b8dfe
nataliaL <sta491@yandex.ru>
2023-07-05 14:33:13 +0300

    add week days

---
+++ TaskDays/Program.cs
@@ -0,0 +1,33 @@
+﻿Console.Write("Введите число от 1 до 7: ");
+int number = int.Parse(Console.ReadLine());
+
+if (number == 1)
+{
+    Console.WriteLine("Понедельник");
+}
+ else if (number == 2)
+{
+    Console.WriteLine("Вторник");
+}
+else if (number == 3)
+{
+    Console.WriteLine("Среда");
+}
+else if (number == 4)
+{
+    Console.WriteLine("Четверг");
+}
+else if (number == 5)
+{
+    Console.WriteLine("Пятница");
+}
+else if (number == 6)
+{
+    Console.WriteLine("Суббота");
+}
+else if (number == 7)
+{
+    Console.WriteLine("Воскресенье");
+}
+else
+    Console.WriteLine("Сказано же, введите число от 1 до 7");
