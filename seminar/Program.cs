4b04d5b237d79bdf2dec887de1fc78755d3bd4db
nataliaL <sta491@yandex.ru>
2023-07-14 13:13:38 +0300

    add sum

---
+++ seminar/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая принимает на вход число (А) и выдаёт сумму чисел от 1 до А.
+// 7 -> 28
+// 4 -> 10
+// 8 -> 36
+
+int GetSum(int limit)
+{
+    int summa = 0;
+    for (int i = 1; i <= limit; i++)
+    {
+        summa += i;
+    }
+    return summa;
+}
+
+int GetInput(string text)
+{
+    Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int numberA = GetInput("Введите число A:");
+Console.WriteLine($"Сумма чисел от 1 до {numberA} ровна {GetSum(numberA)}");
