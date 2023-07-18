f2c65e5fdcff358ccc78796a8d7c6508930318d0
nataliaL <sta491@yandex.ru>
2023-07-18 15:13:27 +0300

    replace

---
+++ ArrayExchange/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу замена элементов массива: положительные элементы замените на соответствующие отрицательные, и наоборот.
+// [-4, -8, 8, 2] -> [4, 8, -8, -2]
+
+int[] ReplaceArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = array[i] * -1;
+}
+return array;
+}
+
+int[] GenerateRandomArray(int elements, int start, int finish)
+{
+    int[] mas = new int[elements];
+    for (int i = 0; i < elements; i++)
+    {
+        mas[i] = new Random().Next(start, finish);
+    }
+    return mas;
+}
+
+int[] array = GenerateRandomArray(12, -9, 9);
+Console.Write($"Изначальный массив [{String.Join(",", array)}]");
+int[] invertArray = ReplaceArray(array);
+Console.WriteLine($"Инвертированный массив [{String.Join(",", invertArray)}]");
