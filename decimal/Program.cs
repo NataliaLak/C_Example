02293d089f831593e0b7dfb0fbc5814289575fe7
nataliaL <sta491@yandex.ru>
2023-07-22 10:30:21 +0300

    add converse to decimal

---
+++ decimal/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 42: Напишите программу, которая будет преобразовывать
+// десятичное число в двоичное.
+// 45 -> 101101
+// 3 -> 11
+// 2 -> 10
+
+void DoBoolean(int number)
+{
+    Console.Write(number + " -> ");
+    string text = String.Empty;
+    while (number > 0)
+    {
+        if (number % 2 == 1) text = "1" + text;
+        else text = "0" + text;
+        number /= 2;
+    }
+    Console.Write(text);
+}
+
+int GetInput(string text)
+{
+    Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int number = GetInput("Введите число: ");
+DoBoolean(number);
