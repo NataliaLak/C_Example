40cfa07b5a8f13788bdc0fb7e026d3665b79821e
nataliaL <sta491@yandex.ru>
2023-07-22 16:09:34 +0300

    add fibonacci rekursiya method

---
+++ RecursiyaFibonacci/Program.cs
@@ -0,0 +1,10 @@
+﻿double Fibonacci(int n)
+{
+    if (n == 1 || n == 2) return 1;
+    else return Fibonacci(n - 1) + Fibonacci(n - 2);
+}
+
+for (int i = 1; i < 50; i++) // ввести нужное число i < 
+{
+    Console.WriteLine(Fibonacci(i));
+}
