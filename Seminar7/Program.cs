7a743923435e643099f0f076af9dfbb630b97d13
nataliaL <sta491@yandex.ru>
2023-07-25 13:48:22 +0300

    add matrix

---
+++ Seminar7/Program.cs
@@ -0,0 +1,47 @@
+﻿// Задача 46: Задайте двумерный массив размером m×n, заполненный случайными целыми числами.
+// m = 3, n = 4.
+// 1 4 8 19
+// 5 -2 33 -2
+// 77 3 8 1
+
+// Задача 46: Задайте двумерный массив размером m×n, заполненный случайными целыми числами.
+// m = 3, n = 4.
+// 1 4 8 19
+// 5 -2 33 -2
+// 77 3 8 1
+
+void Print2dMassive(int[,] massive)
+{
+    for (int i = 0; i < massive.GetLength(0); i++)
+    {
+        for (int j = 0; j < massive.GetLength(1); j++)
+        {
+            Console.Write($"{massive[i, j]}\t ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] Create2dMassive(int rows, int colums, int startValue, int finishValue)
+{
+    int[,] matrix = new int[rows, colums];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(startValue, finishValue + 1);
+        }
+    }
+    return matrix;
+}
+
+int GetInput(string text)
+{
+    Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int m = GetInput("Введите количество строк массива: ");
+int n = GetInput("Введите количество столбцов массива: ");
+int[,] massive = Create2dMassive(m, n, -99, 99);
+Print2dMassive(massive);
