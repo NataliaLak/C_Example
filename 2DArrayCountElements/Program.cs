7d25037d0c6ba52cc06bd6f8f16a1ce877c54460
nataliaL <sta491@yandex.ru>
2023-07-28 14:41:20 +0300

    add count elements

---
+++ 2DArrayCountElements/Program.cs
@@ -0,0 +1,193 @@
+﻿Andrey, [28.07.2023 13:52]
+// Задача 55: Задайте двумерный массив. Напишите программу, которая заменяет строки на столбцы.
+//  В случае, если это невозможно, программа должна вывести сообщение для пользователя.
+
+void SwapRowsToColumns(int[,] array)
+{
+    if (array.GetLength(0) != array.GetLength(1)) Console.Write("Заменить строки на столбцы невозможно: ");
+    else 
+    {
+        int count = 0;
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            for (int j = count; j < array.GetLength(1); j++)
+            {
+                int temp = array[i,j];
+                array[i,j] = array [j,i];
+                array[j,i] = temp;
+            }
+            count++;
+        }
+        Print2DArray(array);
+    }
+}
+
+void Print2DArray (int [,] array)
+{
+    Console.WriteLine("Новый массив выглядит так:");
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(" " + array[i,j]);
+        }
+    Console.WriteLine();
+    }
+}
+
+int[,] Create2DArray (int rows, int columns)
+{
+    int[,] array = new int[rows, columns];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            array[i,j] = new Random().Next (1,100);
+            Console.Write(" " + array[i,j]);
+        }
+    Console.WriteLine();
+    }
+    return array;
+}
+
+int GetInput (string text)
+{
+    Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int rows = GetInput("Введите число строк: ");
+int columns = GetInput("Введите число столбцов: ");
+int [,] array = Create2DArray(rows, columns);
+SwapRowsToColumns(array);
+
+Andrey, [28.07.2023 14:12]
+// Задача 53: Задайте двумерный массив. Напишите программу, которая поменяет местами первую и последнюю строку массива.
+
+void SwapFirstLastRows(int[,] array)
+{
+    {
+        int i = 0;
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            int temp = array[i,j];
+            array[i,j] = array[array.GetLength(0)-1,j];
+            array[array.GetLength(0)-1,j] = temp;
+        }
+        Print2DArray(array);
+    }
+}
+
+void Print2DArray (int [,] array)
+{
+    Console.WriteLine("Новый массив выглядит так:");
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(" " + array[i,j]);
+        }
+    Console.WriteLine();
+    }
+}
+
+int[,] Create2DArray (int rows, int columns)
+{
+    int[,] array = new int[rows, columns];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            array[i,j] = new Random().Next (1,100);
+            Console.Write(" " + array[i,j]);
+        }
+    Console.WriteLine();
+    }
+    return array;
+}
+
+int GetInput (string text)
+{
+    Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int rows = GetInput("Введите число строк: ");
+int columns = GetInput("Введите число столбцов: ");
+int [,] array = Create2DArray(rows, columns);
+SwapFirstLastRows(array);
+
+Andrey, [28.07.2023 14:38]
+// Задача 57: Составить частотный словарь элементов двумерного массива. Частотный словарь содержит информацию о том, сколько раз встречается элемент входных данных.
+// { 1, 9, 9, 0, 2, 8, 0, 9 }
+// 0 встречается 2 раза 
+// 1 встречается 1 раз 
+// 2 встречается 1 раз 
+// 8 встречается 1 раз 
+// 9 встречается 3 раза
+// 1, 2, 3 
+// 4, 6, 1 
+// 2, 1, 6
+// 1 встречается 3 раза 
+// 2 встречается 2 раз 
+// 3 встречается 1 раз 
+// 4 встречается 1 раз 
+// 6 встречается 2 раза
+
+void CountElement(int[,] array)
+{
+    for (int element = 0; element < 10 ; element++)
+    {
+        int count = 0;
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+                if(array[i,j] == element)
+                {
+                    count ++;
+                }
+            }
+        }
+        if (count != 0) Console.WriteLine(element + " встречается " + count + " раз");
+    }
+}
+
+void Print2DArray (int [,] array)
+{
+    Console.WriteLine("Новый массив выглядит так:");
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(" " + array[i,j]);
+        }
+    Console.WriteLine();
+    }
+}
+
+int[,] Create2DArray (int rows, int columns)
+{
+    int[,] array = new int[rows, columns];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            array[i,j] = new Random().Next (1,10);
+            Console.Write(" " + array[i,j]);
+        }
+    Console.WriteLine();
+    }
+    return array;
+}
+
+int GetInput (string text)
+{
+    Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int rows = GetInput("Введите число строк: ");
+int columns = GetInput("Введите число столбцов: ");
+int [,] array = Create2DArray(rows, columns);
+CountElement(array);
