9075ba3a7d83ad2a6f4edf5d39c21735d579211f
nataliaL <sta491@yandex.ru>
2023-08-01 14:32:21 +0300

    add sum digit

---
+++ RecursiyaSumDigit/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 67: Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр.
+// 453 -> 12
+// 45 -> 9
+
+int GetSumDigit(int number)
+{
+    if (number == 0) return number;
+    return number % 10 + GetSumDigit(number / 10);
+}
+
+Console.WriteLine("Введите N:");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"{number} -> {GetSumDigit(number)}");
