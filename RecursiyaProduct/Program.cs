2c08b0acce7678060b695dacff7f7535ca576c73
nataliaL <sta491@yandex.ru>
2023-08-01 14:53:01 +0300

    add product

---
+++ RecursiyaProduct/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 69: Напишите программу, которая на вход принимает два числа A и B, и возводит число А в целую степень B с помощью рекурсии.
+// A = 3; B = 5 -> 243 (3⁵)
+// A = 2; B = 3 -> 8
+
+int GetProduct(int number, int power)
+{
+    if (power == 0) return 1;
+    return number * GetProduct(number, power -1);
+}
+
+Console.Write("Введите A:");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите B:");
+int number2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"{number1} -> {GetProduct(number1, number2)}");
