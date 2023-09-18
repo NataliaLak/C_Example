ad058e18ba379d4c1f0d3accc842e741d6c38faf
nataliaL <sta491@yandex.ru>
2023-09-18 18:32:10 +0300

    deletion

--- Array2Array/Program.cs
+++ Array2Array/Program.cs
@@ -34,3 +34,56 @@ int[] array = GenerateArray(5, -9, 10);
 Console.WriteLine($"[{String.Join(",", array)}]"); 
 int [] newArray = MultiplyElementsOfArray(array); 
 Console.WriteLine($"[{String.Join(",", newArray)}]");
+
+// Сортировка Выбором O(n^2) 
+
+/*
+[45, 23, -10, 5, 3, 9, 1]
+min = -10
+[-10, 23, 45, 5, 3, 9, 1]
+min = 1
+[-10, 1, 45, 5, 3, 9, 23]
+min = 3
+[-10, 1, 3, 5, 45, 9, 23]
+min = 5
+[-10, 1, 3, 5, 45, 9, 23]
+min = 9
+[-10, 1, 3, 5, 9, 45, 23]
+min = 23
+[-10, 1, 3, 5, 9, 23, 45]
+*/
+void InputArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        array[i] = new Random().Next(-20, 21);  // [-20; 20]
+}
+
+
+int[] sortVibor(int[] array)
+{
+    for (int i = 0; i < array.Length; i++) // Счетчики цикла лучше всего обзывать i, j, k, m, n
+    {
+        int indexMin = i;
+        for (int j = i; j < array.Length; j++)
+        {
+            if (array[j] < array[indexMin])
+                indexMin = j;
+        }
+        if (array[indexMin] == array[i])
+            continue;
+        
+        int temp = array[i];
+        array[i] = array[indexMin];
+        array[indexMin] = temp;
+    }
+    return array;
+}
+
+
+Console.Clear();
+Console.Write("Введите кол-во элементов массива: ");
+int n = int.Parse(Console.ReadLine()!);
+int[] array = new int[n];
+InputArray(array);
+Console.WriteLine($"Начальный массив: [{string.Join(", ", array)}]");
+Console.WriteLine($"Конечный массив: [{string.Join(", ", sortVibor(array))}]");
